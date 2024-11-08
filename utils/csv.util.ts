4e931fd81e451d3a37c8e0544ae28096c9f49809
Gaurav Kumar <gauravrajput44@gmail.com>
2024-11-08 21:33:02 +0100

    feat: Add utils and updated yarn package along with yarn.lock

---
+++ utils/csv.util.ts
@@ -0,0 +1,11 @@
+
+export async function getTotalRevenuefromCsv(csvFile: string): Promise<number> {
+    return csvFile
+        .split("\n")
+        .slice(1) // Skip header row
+        .reduce((acc, line) => {
+            const values = line.split(",");
+            const revenue = values[5] ? Number(values[5]) : 0; // Check if the value exists
+            return acc + revenue;
+        }, 0);
+}
